23f8fe63cc53cc148c5788c823b3097d27d2cfc0
Charles Gabriel Gavino <charlesgabrielgavino@chucks-MacBook-Air.local>
2026-02-25 12:25:27 +0800

    update

--- js/slider.js
+++ js/slider.js
@@ -6,4 +6,12 @@
     speed: 200,
     controls: false,
     navPosition: 'bottom',
+    responsive: {
+      1250: {
+        items: 3,
+      },
+      200: {
+        items: 1,
+      },
+    },
   });
